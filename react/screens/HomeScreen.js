87dff5f8345c5badaf02d8e54b58f32dce8d783c
Angelo <locomain12@gmail.com>
2019-05-11 15:01:36 +0200

    react test

---
+++ react/screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as view from './views/HomeScreenView';
+
+export default class HomeScreen extends React.Component {
+
+  render() {
+    return view();
+  }
+}
---
+++ react/screens/views/HomeScreenView.js
@@ -0,0 +1,14 @@
+import {Text, View} from "react-native";
+import React from "react";
+
+
+
+export default function() {
+    return (
+        <View style={styles.container}>
+            <Text>
+                hey giel wat gaaf
+            </Text>
+        </View>
+    );
+}
